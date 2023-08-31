4505d40c7fc0c6a6df1dbe76a493fbc2de3a1074
soma <soma@koii.network>
2023-08-31 11:33:00 -0300

    update backup keywords and turn to headless new

--- adapters/twitter/twitter.js
+++ adapters/twitter/twitter.js
@@ -67,7 +67,7 @@ class Twitter extends Adapter {
     const stats = await PCR(options);
 
     this.browser = await stats.puppeteer.launch({
-      headless: false,
+      headless: 'new',
       userAgent:
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
       args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu'],
